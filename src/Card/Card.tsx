9a552a573015d479528504bbd6b8e9910e35dfa1
alzdirty <zackyalbaehaki@gmail.com>
2022-01-29 08:11:08 +0700

    blur background

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -14,11 +14,13 @@ export const Card: React.FunctionComponent<any> = ({ items }: any) => {
 
     return (
         <>
-            {items ? items.map((m: any) => {
+            {items ? items.map((m: any, i: number) => {
                 return (
-                    <div style={{ backgroundImage: `url(${m.Poster})` }} key={m.Id} className="flex backdrop-blur flex-col px-5 py-3 col-span-1">
-                        <img className='h-[250px]  m-auto' src={m.Poster} alt="" />
-                        <p className='invert-[100%]'>{m.Title}</p>
+                    <div style={{ backgroundImage: `url(${m.Poster})` }} key={i} className=" col-span-1">
+                        <div className='flex flex-col px-5 pb-3 pt-5 backdrop-blur'>
+                            <img className='h-[250px]  m-auto' src={m.Poster} alt="" />
+                            <p className='invert-[100%] '>{m.Title}</p>
+                        </div>
                     </div>)
             }) : `tidak ada`}
         </>);
