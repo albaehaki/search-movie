62ae018e33f3a135f18af7abed344700f11e07d7
alzdirty <zackyalbaehaki@gmail.com>
2022-02-23 20:02:55 +0700

    https

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,14 @@ function App() {
 
   const api = (e: any) => {
     axios
-      .get(`https://www.omdbapi.com/?apikey=aa1a78d7&s=${e}`)
+      .get(`HTTPS://www.omdbapi.com/?apikey=aa1a78d7&s=${e}`)
       .then((response) => {
         setUsers(response.data.Search);
       });
   };
   const api2 = (e: any) => {
     axios
-      .get(`https://www.omdbapi.com/?apikey=aa1a78d7&i=${e}`)
+      .get(`HTTPS://www.omdbapi.com/?apikey=aa1a78d7&i=${e}`)
       .then((response) => {
         setPar(response.data);
       });
