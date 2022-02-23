024a0e497a62ddc5afa7b5fe6ef11d017b736e47
alzdirty <zackyalbaehaki@gmail.com>
2022-02-23 19:24:02 +0700

    baru

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Search from "./search/Search";
 import { Card } from "./Card/Card";
 import { Detail } from "./detail/Detail";
 import axios from "axios";
-import { type } from "os";
 import { Route, Routes } from "react-router-dom";
-
-// interface User {
-//   id: number;
-//   firstName: string;
-// }
-
-// export type TUserList = User[]
 
 function App() {
   const [users, setUsers] = useState<any>([]);
-  const [toogle, setToggle] = useState<boolean>(false);
   const [keyword, setKeyword] = useState<String>();
-  const [id, setId] = useState<String>();
   const [par, setPar] = useState<any>([]);
-
-  // useEffect(() => {
-  //   axios.get(`http://www.omdbapi.com/?apikey=aa1a78d7&s=${keyword}`)
-  //     .then((response) => {
-  //       setUsers(response.data.Search);
-  //     })
-  // }, [])
 
   const api = (e: any) => {
     axios
@@ -44,18 +27,12 @@ function App() {
   };
 
   const ambil = async (e: any) => {
-    //e.preventDefault()
     api2(e.currentTarget.id);
-    //.getAttribute('data-attr')
-    //.previousSibling.previousSibling.
   };
 
   const garux = (e: any) => {
     e.preventDefault();
     api(keyword);
-    //setToggle(!toogle)
-    //console.log(users);
-    //setKeyword(e.target.nextElementSibling.value)
   };
 
   const ubah = (e: any) => {
@@ -74,13 +51,6 @@ function App() {
           <Search change={ubah} click={garux} keyword={keyword} />
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 overflow-x-hidden">
-          {/* {users ? users.map((m: any) => {
-            return (
-              <Card items={m} />
-
-            ) */}
-
-          {/* }) : `Tidak ada `} */}
           <Routes>
             <Route path="detail" element={<Detail items={par} />} />
             <Route path="/" element={<Card items={users} click={ambil} />} />
